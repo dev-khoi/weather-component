3645ae0bad23f497e9cfad31cb09e148dc4460f6
daknguyen-Seneca <daknguyen@myseneca.ca>
2025-05-19 16:50:36 -0400

    using reacdt-grid-layout

--- src/gridLayout.tsx
+++ src/gridLayout.tsx
@@ -1,22 +1,51 @@
-import { useState, useEffect } from "react";
-import { getWeather } from "./weatherAPI.tsx";
-// Grid import
-import "gridstack/dist/gridstack.min.css";
-import { GridStack } from "gridstack";
-const App = () => {
-    useEffect(() => {
-        const grid = GridStack.init();
-        grid.load(serializedData)
-        console.log("grid")
-    },[])
-    const serializedData = [
-        { x: 0, y: 0, w: 2, h: 2, minW:2, minH:2},
-    ];
-
-     return (
-        <>
-        </>
+import type { ReactNode, HTMLAttributes } from "react";
+import GridLayout from "react-grid-layout";
+import { type Layout } from "react-grid-layout";
+import "react-grid-layout/css/styles.css";
+import "react-resizable/css/styles.css";
+interface prop {
+    children: ReactNode;
+    layout?: Layout[];
+}
+const Layout = (prop: prop) => {
+    return (
+        <GridLayout className="layout" cols={12} rowHeight={30} width={1200}>
+            {prop.children}
+        </GridLayout>
     );
 };
 
-export default App;
+// component of layout
+interface Component {
+    children: ReactNode;
+    grid: Layout;
+    className: string;
+}
+const Component = (prop: Component) => {
+    return(
+        <div key={prop.grid?.i} data-grid={prop.grid} className={prop.className}>
+
+        </div>
+    )
+};
+
+export const App = () => {
+    return (
+        <Layout>
+            <Component
+                key="a"
+                grid={{ i: "a", x: 0, y: 0, w: 1, h: 2, static: false }}
+                className="grid-layout-item-content"
+            >
+                a
+            </Component>
+            <Component
+                key="b"
+                grid={{ i: "b", x: 1, y: 1, w: 1, h: 2, static: false }}
+                className="grid-layout-item-content"
+            >
+                b
+            </Component>
+        </Layout>
+    );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import  App  from "./gridLayout.tsx"
+import  {App}  from "./gridLayout.tsx"
 import { navBar } from "./navbar.ts";
 navBar();
 createRoot(document.getElementById("root")!).render(
